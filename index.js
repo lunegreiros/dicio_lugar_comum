506b87d6f4a63d47075ecc17108d8028f5c10dc1
Luciano Negreiros <lunegreiros@gmail.com>
2021-06-07 14:42:49 -0300

    correct api with word and expressions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const mysql = require('mysql2')
 const fs = require('fs')
 require('dotenv').config()
+const expressoesModels = require('../models/expressoesModels');
 
 const port = process.env.PORT || 3000
 const connection = mysql.createConnection(process.env.JAWSDB_URL)
@@ -25,9 +26,38 @@ connection.connect(err => {
 })
 
 app.get('/', (req, res) => {
-  res.send('Dicionário de Lugar Comum')
+  res.send('Dicionário de Lugar Comum - /api/:<_palavra_> ou /api/todas_palavras')
 })
-app.get('/palavras', (req, res) => {
+
+app.get('/api/:palavra', (req, res) => {
+  const palavra = req.params.palavra;
+  const sql = `SELECT id FROM palavras WHERE palavra = '${palavra}'`;
+    connection.query(sql, (erro, resultado) => {
+
+      if(erro){
+        res.status(400).json(erro)
+      }else if(resultado.length === 0){
+        res.status(200).json({"mensagem": "Palavra não existe"});
+      }else{
+        const id = resultado[0].id;
+        const sql = `SELECT * FROM expressoes WHERE id_palavra = ${id}`
+        connection.query(sql, (erro, result) => {
+          if(erro){
+            res.status(400).json(erro)
+          }else{
+            var expressaoArray = [];
+            result.forEach(function(item){
+              expressaoArray.push(item.expressao)
+            });
+            res.status(200).json({"palavra": palavra , "expressoes":expressaoArray})
+          }
+        });
+      }
+
+    });
+})
+
+app.get('/api/todas_palavras', (req, res) => {
   connection.query('select * from palavras', (err, rows) => {
     if (err) {
       res.send({
